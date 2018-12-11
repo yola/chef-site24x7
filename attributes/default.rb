7bf3d8eeb5a26ec6e108991502ffd35e0b00f816
fr33l <andrii.zakharkiv@synthasite.com>
2018-12-11 14:17:33 +0200

    oops, copy-pasta

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,6 @@ default['Site24x7']['devicekey'] = 'YOUR_USER_DEVICE_KEY'
 default['Site24x7']['proxy'] = ''
 
 # package identification
-# package identification
 if node['kernel']['machine'] == 'i386' || node['kernel']['machine'] == 'i686'
   default['package'] = 'Site24x7_Linux_32bit.install'
 else
